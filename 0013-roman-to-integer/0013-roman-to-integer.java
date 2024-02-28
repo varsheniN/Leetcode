1ebfee64779de42ff597dd2e327488d4e13088ac
varsheniN <139369123+varsheniN@users.noreply.github.com>
2024-02-28 21:30:24 +0530

    Time: 2 ms (100.00%), Space: 44.5 MB (85.49%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int romanToInt(String s) {
+         int i;
+        int ans=0,prev=0,num=0;
+        for(i=s.length()-1;i>=0;i--){
+            switch(s.charAt(i)){
+                case 'I':{
+                    num=1;
+                    break;
+                }
+                case 'V':{
+                    num=5;
+                    break;
+                }
+                case 'X':{
+                    num=10;
+                    break;
+                }    
+                case 'L':{
+                    num=50;
+                    break;
+                }
+                case 'C':{
+                    num=100;
+                    break;
+                } 
+                case 'D':{
+                    num=500;
+                    break;
+                } 
+                case 'M':{
+                    num=1000;
+                    break;
+                }
+            }
+                if(num<prev){
+                    ans-=num;
+                }    
+                 else{
+                     ans+=num;
+                 }
+                    prev=num;
+            }
+        return ans;
+        
+    }
+}
